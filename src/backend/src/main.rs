ea23d48994f4d4b92705d98b8559fdc59f9c1dce
Erritis <serg.shehov@tutanota.com>
2023-02-16 22:38:23 +0300

    Revert "Replace authorization library with aliri"
    This reverts commit ab48d8eb0206f9188ec8615b7bf08497a72e36f6.

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,12 +1,10 @@
 
 mod endpoints;
 
+use actix_4_jwt_auth::{Oidc, OidcBiscuitValidator, biscuit::{ValidationOptions, Validation}, OidcConfig};
 use actix_web::{middleware::Logger, App, HttpServer, web};
 use actix_cors::Cors;
 
-
-use aliri::jwt;
-use aliri_oauth2::{Authority};
 use endpoints::{healthcheck, userinfo};
 
 
@@ -15,18 +13,16 @@ async fn main() -> std::io::Result<()> {
 
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     
-    let issuer = std::env::var("ISSUER").expect("ISSUER must be set");
+    let authority = std::env::var("ISSUER").expect("ISSUER must be set");
 
-    let validator = jwt::CoreValidator::default()
-            .require_issuer(jwt::Issuer::new(issuer.clone()))
-            //.add_allowed_audience(jwt::Audience::new(issuer.clone()))
-            ;
-    // To get jwks_uri look in {issuer}/.well-known/openid-configuration
-    let jwks_url = format!("{}/protocol/openid-connect/certs", issuer);
+    let oidc = Oidc::new(OidcConfig::Issuer(authority.clone().into())).await.unwrap();
 
-    let authority = Authority::new_from_url(jwks_url, validator)
-    .await.expect("AUTHORITY didn't create");
-
+    let biscuit_validator = OidcBiscuitValidator { options: ValidationOptions {
+            issuer: Validation::Validate(authority),
+            ..ValidationOptions::default()
+        }
+    };
+    //let scope_validator = OidcScopeValidator(vec!["openid", "profile"]);
 
     HttpServer::new(move || {
         let cors = Cors::default().allow_any_origin().allow_any_header().allow_any_method().supports_credentials();
@@ -36,7 +32,10 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .service(
                 web::scope("")
-                .app_data(authority.clone())
+                .app_data(oidc.clone())
+                //.wrap(scope_validator.clone())
+                .wrap(biscuit_validator.clone())
+                //.wrap(OidcBiscuitValidator::default())
                 .wrap(cors)
                 .service(userinfo)
                 .service(healthcheck)
