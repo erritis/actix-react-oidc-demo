e69f516ab949d4308e012713aaae38b3945caff5
Erritis <serg.shehov@tutanota.com>
2023-02-16 22:38:23 +0300

    Revert "Replace authorization library with aliri"
    This reverts commit ab48d8eb0206f9188ec8615b7bf08497a72e36f6.

--- src/backend/src/endpoints.rs
+++ src/backend/src/endpoints.rs
@@ -1,5 +1,6 @@
+use actix_4_jwt_auth::{AuthenticatedUser};
 use actix_web::{get, HttpResponse};
-use aliri_actix::scope_policy;
+use serde::{Serialize, Deserialize};
 
 
 #[get("/healthcheck")]
@@ -8,11 +9,19 @@ async fn healthcheck() -> HttpResponse {
 }
 
 
-
-scope_policy!(Profile / ProfileScope; "profile");
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct FoundClaims {
+    pub iss: String,
+    pub sub: String,
+    pub aud: String,
+    pub name: String,
+    pub email: Option<String>,
+    pub email_verified: Option<bool>,
+    pub scope: String,
+}
 
 
 #[get("/userinfo")]
-pub async fn userinfo(user: Profile) -> HttpResponse {
-    HttpResponse::Ok().json(user.claims())
+pub async fn userinfo(user: AuthenticatedUser<FoundClaims>) -> HttpResponse {
+    HttpResponse::Ok().json(user.claims)
 }
